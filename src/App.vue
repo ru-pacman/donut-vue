98ca9110b92a0863442cd652d0b30592c8bfdaf5
Dmitry Reshetnikov <>
2020-05-26 14:20:25 +0300

    Add Donut Chart
    - Add new component and styles
    - Delete `src/components/HelloWorld.vue`
    - Change styles in `src/App.vue`

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <label>
+      <input type="checkbox" v-model="isFunnyColor" />
+      Радужные цвета
+    </label>
+    <Donut
+      :slices="slices"
+      :currentSlice="currentSlice"
+      :colors="colors"
+      @click="currentSlice = $event"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Donut from "./components/donut/";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Donut
+  },
+  data: () => ({
+    slices: [
+      { id: 1, text: "79200101623", percent: 0.37 },
+      { id: 2, text: "79200101623", percent: 0.16 },
+      { id: 3, text: "79200101623", percent: 0.12 },
+      { id: 4, text: "79200101623", percent: 0.11 },
+      { id: 5, text: "79200101623", percent: 0.1 },
+      { id: 6, text: "79200101623", percent: 0.05 },
+      { id: 7, text: "79200101623", percent: 0.03 },
+      { id: 8, text: "79200101623", percent: 0.02 },
+      { id: 9, text: "79200101623", percent: 0.01 },
+      { id: 10, text: "79200101623", percent: 0.01 },
+      { id: 11, text: "79200101623", percent: 0.01 },
+      { id: 12, text: "79200101623", percent: 0.01 }
+    ],
+    currentSlice: 10,
+    funnyColors: [
+      "#67B6F4",
+      "#ce67f4",
+      "#f88888",
+      "#ffb57d",
+      "#9ddc5d",
+      "#5ddcb0",
+      "#7b94e0",
+      "#e671ad",
+      "#c37252",
+      "#c39a52",
+      "#b2c352",
+      "#52c358",
+      "#52c3b6"
+    ],
+    boringColors: [
+      "#67B6F4",
+      "#67E3F4",
+      "#9E88F8",
+      "#7D8AFF",
+      "#5DA4DC",
+      "#5DCDDC",
+      "#8E7BE0",
+      "#717CE6",
+      "#5292C3",
+      "#52B6C3",
+      "#7E6DC6",
+      "#646ECC",
+      "#487FAA",
+      "#489EAA",
+      "#6E5FAD",
+      "#5760B2",
+      "#3D6D92",
+      "#3D8792",
+      "#5E5194",
+      "#4B5298",
+      "#335A79",
+      "#337079",
+      "#4E437B",
+      "#3E447E"
+    ],
+    isFunnyColor: true
+  }),
+  computed: {
+    colors() {
+      return this.isFunnyColor ? this.funnyColors : this.boringColors;
+    }
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Helvetica, Arial, sans-serif;
+  color: #333;
+  margin: 60px auto;
+  width: 100%;
+  max-width: 600px;
+}
+label {
+  display: block;
+  margin-bottom: 2em;
+  width: fit-content;
+}
+svg {
+  outline: 1px dashed #8000ff;
 }
 </style>
---
+++ src/components/donut/index.js
@@ -0,0 +1,166 @@
+import styles from "./styles.module.css";
+
+const COLORS = [
+  "#67B6F4",
+  "#67E3F4",
+  "#9E88F8",
+  "#7D8AFF",
+  "#5DA4DC",
+  "#5DCDDC",
+  "#8E7BE0",
+  "#717CE6",
+  "#5292C3",
+  "#52B6C3",
+  "#7E6DC6",
+  "#646ECC",
+  "#487FAA",
+  "#489EAA",
+  "#6E5FAD",
+  "#5760B2",
+  "#3D6D92",
+  "#3D8792",
+  "#5E5194",
+  "#4B5298",
+  "#335A79",
+  "#337079",
+  "#4E437B",
+  "#3E447E"
+];
+const SIZES = {
+  viewBox: {
+    width: 600,
+    height: 400
+  },
+  normal: {
+    bigRadius: 148,
+    smallRadius: 72,
+    textRadius: 168,
+    percentLabelRadius: 110
+  },
+  hover: {
+    bigRadius: 158,
+    smallRadius: 72,
+    textRadius: 168,
+    percentLabelRadius: 110
+  }
+};
+
+function getCoordinatesForPercent(percent) {
+  const x = Math.cos(2 * Math.PI * percent);
+  const y = Math.sin(2 * Math.PI * percent);
+  return [x, y];
+}
+
+export default {
+  name: "Donut",
+  props: {
+    slices: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
+    colors: {
+      type: Array,
+      required: false,
+      default: () => COLORS
+    },
+    sizes: {
+      type: Object,
+      required: false,
+      default: () => SIZES
+    },
+    currentSlice: {},
+    visible: true
+  },
+  methods: {
+    sliceClick(id) {
+      this.$emit("click", id);
+    }
+  },
+  render() {
+    let cumulativePercent = -0.25; // Чтобы части начинали свое расположения на 12 часов;
+
+    return (
+      <svg
+        viewBox={`-${this.sizes.viewBox.width / 2} -${this.sizes.viewBox
+          .height / 2} ${this.sizes.viewBox.width} ${
+          this.sizes.viewBox.height
+        }`}
+        style={{
+          width: "100%",
+          height: "100%",
+          maxWidth: this.sizes.viewBox.width,
+          maxHeight: this.sizes.viewBox.height
+        }}
+      >
+        <text x="0" y="0" text-anchor="middle" class={styles.svgTextAll}>
+          Всего (шт.):
+        </text>
+        <text x="0" y="14" text-anchor="middle" class={styles.svgTextAll}>
+          546864
+        </text>
+
+        {this.slices.map((slice, key) => {
+          const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
+          const [textX, textY] = getCoordinatesForPercent(
+            cumulativePercent + slice.percent / 2
+          );
+          cumulativePercent += slice.percent;
+          const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
+          const largeArc = slice.percent > 0.5 ? 1 : 0;
+          const size = this.sizes[
+            slice.id === this.currentSlice ? "hover" : "normal"
+          ];
+
+          const pathData = [
+            `M ${startX * size.bigRadius} ${startY * size.bigRadius}`, // Координаты начальной точки
+            `A ${size.bigRadius} ${size.bigRadius} 0 ${largeArc} 1 ${endX *
+              size.bigRadius} ${endY * size.bigRadius}`, // Кривая
+            `L ${endX * size.smallRadius} ${endY * size.smallRadius}`, // Линия
+            `A ${size.smallRadius} ${
+              size.smallRadius
+            } 0 ${largeArc} 0 ${startX * size.smallRadius} ${startY *
+              size.smallRadius}`, // Кривая
+            "Z" // Закрыть полигон
+          ].join(" ");
+          return (
+            <g id={slice.id}>
+              <path
+                d={pathData}
+                fill={this.colors[key % this.colors.length]}
+                vector-effect="non-scaling-stroke"
+                class={styles.svgSlice}
+                vOn:click={() => this.sliceClick(slice.id)}
+              />
+              {slice.percent >= 0.05 && (
+                <g>
+                  <text
+                    x={textX * size.textRadius}
+                    y={textY * size.textRadius + 8}
+                    text-anchor={
+                      (cumulativePercent - slice.percent / 2 + 0.25) % 1 > 0.5
+                        ? "end"
+                        : "start"
+                    }
+                    class={styles.svgText}
+                  >
+                    {slice.text}
+                  </text>
+                  <text
+                    x={textX * size.percentLabelRadius}
+                    y={textY * size.percentLabelRadius + 6}
+                    text-anchor="middle"
+                    class={styles.svgTextPercent}
+                    vOn:click={() => this.$emit("click", slice.id)}
+                  >
+                    {slice.percent * 100}%
+                  </text>
+                </g>
+              )}
+            </g>
+          );
+        })}
+      </svg>
+    );
+  }
+};
